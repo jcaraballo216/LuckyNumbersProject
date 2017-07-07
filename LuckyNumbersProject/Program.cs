74ed0584f8fe8ff3ba043348e8667846ad9fbc14
Jerome <jscar216@gmail.com>
2017-07-07 16:45:06 -0400

    final touches

--- LuckyNumbersProject/Program.cs
+++ LuckyNumbersProject/Program.cs
@@ -11,56 +11,97 @@ namespace LuckyNumbersProject
         static void Main(string[] args)
         //Asking user for info
         {
-            Console.WriteLine("Weclome to Jerome's Lottery");
-            Console.WriteLine("You will be asked to enter two numbers to start");
-            Console.WriteLine("Please enter your first number");
+            Console.WriteLine("Weclome to Jerome's Lottery!");
+            Console.WriteLine("The JackPot amount is currently 120,000.");
+            Console.WriteLine("You will be asked to enter two numbers to start.");
+            Console.WriteLine("Please enter your first number.");
             int firstNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter your second number");
+            Console.WriteLine("Please enter your second number.");
             int secondNum = int.Parse(Console.ReadLine());
-            Console.WriteLine("Now you will be asked to input 6 numbers one at a time");
+            Console.WriteLine("Now you will be asked to input 6 numbers one at a time.");
             //setting the numbers in the array
             int[] luckyNumbers = new int[6];
             for (int i = 0; i <= luckyNumbers.Length - 1; i++)
             {
-                Console.WriteLine("Enter the number");
+                Console.WriteLine("Enter your number.");
                 luckyNumbers[i] = int.Parse(Console.ReadLine());
                 while (luckyNumbers[i] < firstNumber || luckyNumbers[i] > secondNum)
                 {
 
-                    Console.WriteLine("Enter a valid number");
+                    Console.WriteLine("Enter a valid number!");
                     break;
                 }
+
             }
+            //setting random in array
+            Random number = new Random();
+            int[] numbers = new int[6];
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = number.Next(firstNumber, secondNum);
+            }
+            foreach (int numOne in numbers)
+            {
+                Console.WriteLine("Your number: " + (numOne));
+            }
+            int jackPot = 120000;
+            int lottoNumber = 0;
+            //hardcode for jackpot
+            for (int i = 0; i < luckyNumbers.Length; i++) 
+            {
+                foreach (int num in luckyNumbers)
+                    if (num == numbers[i]) 
+                {
+                        lottoNumber ++;
+                }
+                
+            }
+            Console.WriteLine("You guessed " + lottoNumber + " correctly");
+            if (lottoNumber == 6)
+            {
+                Console.WriteLine("Congrats you have won the JackPot in the amount of " + jackPot);
+            }
+            else if (lottoNumber >=4)
+            {
+                Console.WriteLine("Congrats you have won $" + jackPot * .75);
+            }
+            else if (lottoNumber >= 2)
+            {
+                Console.WriteLine("Congrats you have won $" + jackPot * .25);
+            }
+            else if (lottoNumber >= 0)
+            {
+                Console.WriteLine("You didn't pick that many numbers but you still win $1.00.");
+            }
+            //making a do-while loop for yes/no question
+            string lottoDec;
+            do
+            {
+                Console.WriteLine("Thank you for playing! Would you like to play again! yes/no");
+                lottoDec = Console.ReadLine().ToLower();
+            }
+            while (lottoDec == "no");
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+            
            
-
-            
-            
-            
-            
-            // If the user enters a number that is outside of the range set, prompt the user to give you a valid number. Do this until the user enters a valid number.
-            //Part 2
-
-            // The program should randomly generate 6 numbers using a loop
-
-       
-            // The randomly generated numbers should be stored in an array
-            // Numbers should be displayed to the console as such and using a loop (Numbers below are for example only. Format must be exact):
-            //Lucky Number: 12
-            //Lucky Number: 47
-            //Lucky Number: 28
-            //Lucky Number: 3
-            //Lucky Number: 19
-            //Lucky Number: 35
-            //Part 3
-
-            // Hard - code a value for the jackpot amount and let the user know what the jackpot amount is at some point you decide in the program.
-            //   The program should count the number of correctly guessed numbers and output to the console to notify the user.Example:
-            //You guessed 3 numbers correctly!
-            //   The program should calculate the user's winnings based on the number of numbers guessed correctly.
-
-            //   The user's winnings should be output to the console. Example:
-            //  You won $256, 877.23!
             //  Part 4
 
 
